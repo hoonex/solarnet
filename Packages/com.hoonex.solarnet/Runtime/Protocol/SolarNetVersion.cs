3aea7055434f760da9b3b2800cfcc8d45af8dac8
hoonex <163626004+hoonex@users.noreply.github.com>
2026-09-08 22:20:27 +0900

    Align SolarNet engine version to 0.17.0

--- Packages/com.hoonex.solarnet/Runtime/Protocol/SolarNetVersion.cs
+++ Packages/com.hoonex.solarnet/Runtime/Protocol/SolarNetVersion.cs
@@ -2,7 +2,7 @@ namespace SolarNet.Protocol
 {
     public static class SolarNetVersion
     {
-        public const string EngineVersion = "0.16.0";
+        public const string EngineVersion = "0.17.0";
         public const byte CoreProtocolVersion = SolarPacket.CurrentProtocolVersion;
         public const byte RoomProtocolVersion = 1;
     }
